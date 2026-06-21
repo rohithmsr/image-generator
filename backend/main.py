cf196c60bfdd7abd10cccdfc252c8dec81b0da82
rohithmsr <rohit.rajaram@arcadia.com>
2026-06-21 15:56:05 +0530

    feat: initialize FastAPI application with lifespan database setup, CORS middleware, and routing support, and ignore .env files

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from backend!")
+from routes import router
+import logging
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+from database import create_db_and_tables
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logging.info("Starting server...")
+    create_db_and_tables()
+    yield
+    logging.info("Stopping server...")
+
+app = FastAPI(
+    title="Image Generator with FastAPI",
+    lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], # just for local development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
